142a72f5fd99723d1665c28f453bee6c04385542
peoplewhite <shonshon7@gmail.com>
2017-01-24 19:18:28 +0800

    [add] add textfield to type car plate number before start tracing

--- TraceMyRoutes/FirstSceneViewController.swift
+++ TraceMyRoutes/FirstSceneViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class FirstSceneViewController: UIViewController {
+    
+    @IBOutlet weak var trackButton: UIButton!
+
+    @IBOutlet weak var typeCarPlateNumberTextfield: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +20,22 @@ class FirstSceneViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: false)
 
     }
+    override func viewWillAppear(_ animated: Bool) {
+
+        typeCarPlateNumberTextfield.becomeFirstResponder()
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
+    @IBAction func typeCarPlateNumberEditingChanged(_ sender: UITextField) {
+        trackButton.isEnabled = (sender.text?.characters.count)! > 0
+
+
+    }
+
 
 
 }
