be18f143154c9b5e51e4b909bf35926f2115a841
peoplewhite <shonshon7@gmail.com>
2017-02-02 11:30:49 +0800

    [add] add components on screen

---
+++ TraceMyRoutes/WarningScene.swift
@@ -0,0 +1,15 @@
+//
+//  WarningScene.swift
+//  TraceMyRoutes
+//
+//  Created by sean on 2017/2/2.
+//  Copyright © 2017年 oddesign. All rights reserved.
+//
+
+import UIKit
+
+class WarningScene: UIView {
+
+    
+
+}
