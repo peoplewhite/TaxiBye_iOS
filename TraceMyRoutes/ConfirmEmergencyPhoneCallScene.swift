be869b703faaf06ffe09b94c80316a0473753988
peoplewhite <shonshon7@gmail.com>
2017-02-06 18:25:59 +0800

    [add] create ConfirmEmergencyPhoneCallScene nib and add components on it

---
+++ TraceMyRoutes/ConfirmEmergencyPhoneCallScene.swift
@@ -0,0 +1,21 @@
+//
+//  ConfirmEmergencyPhoneCallScene.swift
+//  TraceMyRoutes
+//
+//  Created by sean on 2017/2/6.
+//  Copyright © 2017年 oddesign. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmEmergencyPhoneCallScene: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
